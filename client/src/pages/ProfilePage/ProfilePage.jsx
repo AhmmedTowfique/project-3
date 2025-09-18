13e0bd4866e4cab24482789c13371a9650ea3e94
AhmmedTowfique <ahmmed.towfique@gmail.com>
2025-09-18 11:02:25 +0200

    CI/CD test

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
-import Confetti from "react-confetti";
-
 function ProfilePage() {
-  const [showConfetti, setShowConfetti] = useState(true);
-
-  useEffect(() => {
-    // Stop confetti after 10 seconds
-    const timer = setTimeout(() => setShowConfetti(false), 10000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div>
       <h1>Profile page</h1>
-      <p>its a CI/CD check</p>
-
-      {showConfetti && <Confetti width={window.innerWidth} height={window.innerHeight} />}
+      <p>its a CI/CD check 11</p>
     </div>
   );
 }
+
 export default ProfilePage;
